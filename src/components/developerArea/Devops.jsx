13e4e335d9aa4ae87ffc8901a2694fa1799ff5c6
Sujith-Rajan <sujithrajan223@gmail.com>
2024-01-20 14:04:13 +0530

    developer detail re-correct

--- src/components/developerArea/Devops.jsx
+++ src/components/developerArea/Devops.jsx
@@ -3,10 +3,10 @@ import styles from './developer.module.scss'
 const Devops = () => {
     const date = new Date();
   const year = date.getFullYear();
-  const developerName = 'Sujith-Rajan';
+ 
   return (
     <div className={styles.container}>
-      <span>&#169;<span>{year}</span>-Run Dev.All right reserved by <span className={styles.developerName}>{developerName}</span></span>
+      <span>&#169;<span>{year}</span>-Run Dev.All right reserved by <span className={styles.developerName}>Sujith Rajan</span></span>
     </div>
   )
 }
